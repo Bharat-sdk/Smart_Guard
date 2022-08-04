3cab4d658c85f528c45e52c92952edd8f814d1a5
mobiledevelopment422 <110315644+mobiledevelopment422@users.noreply.github.com>
2022-08-04 07:00:00 +0530

    "Add required Dependencies for App"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -45,7 +47,33 @@ dependencies {
     implementation(Libs.APP_COMPAT_LIB)
     implementation (Libs.MATERIAL_LIB)
     implementation (Libs.CONSTRAINT_LAYOUT_LIB)
+    implementation(Libs.ROOM_RUN_TIME)
+    annotationProcessor(Libs.ROOM_COMPILER)
     testImplementation(Libs.JUNIT_LIB)
     androidTestImplementation(Libs.JUNIT_EXT_LIB)
     androidTestImplementation(Libs.ESPRESSO_CORE)
+    // ViewModel
+    implementation(Libs.VIEW_MODEL)
+
+    // LiveData
+    implementation(Libs.LIVE_DATA)
+    // Annotation processor
+    kapt(Libs.LIFECYCLE_COMPILER)
+    // alternately - if using Java8, use the following instead of lifecycle-compiler
+    implementation(Libs.LIFECYCELE_JAVA_SUPPORT)
+    //Koin -- Dependency  Injection
+    implementation(Libs.KOIN_CORE)
+    implementation( platform(Libs.FIREBASE_BOM))
+
+    // Koin main features for Android
+    implementation (Libs.KOIN_ANDROID)
+
+// Java Compatibility
+    implementation (Libs.KOIN_COMPAT)
+// Jetpack WorkManager
+    implementation(Libs.KOIN_WORKMANAGER)
+// Navigation Graph
+    implementation(Libs.KOIN_NAVIGATION)
+    implementation(Libs.TIMBER_LIB)
+    debugImplementation(Libs.LEAK_CANARY_LIB)
 }
