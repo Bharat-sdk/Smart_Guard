af334a69ddd570955634146a026ccf4a86ddb251
mobiledevelopment422 <110315644+mobiledevelopment422@users.noreply.github.com>
2022-08-05 03:58:07 +0530

    "Setup Koin and Firebase     CRASHLYTICS"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -20,6 +21,7 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -51,7 +53,7 @@ dependencies {
     implementation (Libs.MATERIAL_LIB)
     implementation (Libs.CONSTRAINT_LAYOUT_LIB)
     implementation(Libs.ROOM_RUN_TIME)
-    annotationProcessor(Libs.ROOM_COMPILER)
+    kapt(Libs.ROOM_COMPILER)
     testImplementation(Libs.JUNIT_LIB)
     androidTestImplementation(Libs.JUNIT_EXT_LIB)
     androidTestImplementation(Libs.ESPRESSO_CORE)
@@ -62,21 +64,21 @@ dependencies {
     implementation(Libs.LIVE_DATA)
     // Annotation processor
     kapt(Libs.LIFECYCLE_COMPILER)
-    // alternately - if using Java8, use the following instead of lifecycle-compiler
+  
     implementation(Libs.LIFECYCELE_JAVA_SUPPORT)
     //Koin -- Dependency  Injection
-    implementation(Libs.KOIN_CORE)
+    // Koin Core features
+    implementation (Libs.KOIN_CORE)
+    implementation(Libs.KOIN_ANDROID)
+    implementation(Libs.COROUTINE_LIB)
+    //Firebase
     implementation( platform(Libs.FIREBASE_BOM))
-
-    // Koin main features for Android
-    implementation (Libs.KOIN_ANDROID)
-
-// Java Compatibility
-    implementation (Libs.KOIN_COMPAT)
-// Jetpack WorkManager
-    implementation(Libs.KOIN_WORKMANAGER)
-// Navigation Graph
-    implementation(Libs.KOIN_NAVIGATION)
+    implementation(Libs.FIREBASE_CRASHLYTICS)
+    //Logging
     implementation(Libs.TIMBER_LIB)
+    //Memory Leak
     debugImplementation(Libs.LEAK_CANARY_LIB)
+    //Multidex
+    implementation(Libs.MULTIDEX_LIB)
+
 }
---
+++ app/src/main/java/com/hbeonlabs/smartguard/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.hbeonlabs.smartguard.di
+
+import org.koin.dsl.module
+
+val appModule =module {
+
+}
