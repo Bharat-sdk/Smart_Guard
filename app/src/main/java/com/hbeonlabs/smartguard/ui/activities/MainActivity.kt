826ddfc2689b34018a68abb2bfb97dafbad72e57
bharat kumar <bharat.kumar@risertechub.in>
2022-08-08 21:57:21 +0530

    Fragment Add A Hub Navigation Done

--- app/src/main/java/com/hbeonlabs/smartguard/ui/activities/MainActivity.kt
+++ app/src/main/java/com/hbeonlabs/smartguard/ui/activities/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.hbeonlabs.smartguard.ui.activities
 
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.hbeonlabs.smartguard.R
 import com.hbeonlabs.smartguard.base.BaseActivity
 import com.hbeonlabs.smartguard.databinding.ActivityMainBinding
 import org.koin.android.ext.android.inject
 
 class MainActivity : BaseActivity<MainViewModel,ActivityMainBinding>() {
-
+    private lateinit var navController: NavController
     private  val mainViewModel: MainViewModel by inject()
     override fun getViewModel(): MainViewModel {
         return mainViewModel
@@ -18,7 +21,35 @@ class MainActivity : BaseActivity<MainViewModel,ActivityMainBinding>() {
 
     override fun initView() {
         super.initView()
+        navController = findNavController(R.id.navHostFragment)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
+
+        hideShowToolbar()
+
+
+    }
+
+
+
+    fun hideShowToolbar()
+    {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.toolbarTitle.text = destination.label
+
+            when (destination.id) {
+                R.id.fragmentSplash -> {
+                    supportActionBar?.hide()
+                }
+                R.id.fragmentOnBoarding -> {
+                    supportActionBar?.hide()
+                }
+                else -> {
+                    supportActionBar?.show()
+                }
+            }
+        }
     }
 
 
---
+++ app/src/main/java/com/hbeonlabs/smartguard/ui/fragments/addAHub/FragmentAddAHub.kt
@@ -0,0 +1,35 @@
+package com.hbeonlabs.smartguard.ui.fragments.addAHub
+
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.hbeonlabs.smartguard.R
+import com.hbeonlabs.smartguard.base.BaseFragment
+import com.hbeonlabs.smartguard.data.local.models.OnBoardingData
+import com.hbeonlabs.smartguard.databinding.FragmentAddAHubBinding
+import com.hbeonlabs.smartguard.databinding.FragmentOnboardingBinding
+import com.hbeonlabs.smartguard.ui.adapters.ViewPagerAdapter
+import com.hbeonlabs.smartguard.ui.fragments.addAHub.AddAHubViewModel
+import com.hbeonlabs.smartguard.ui.fragments.splash.SplashviewModel
+
+import org.koin.android.ext.android.inject
+
+
+class FragmentAddAHub:BaseFragment<AddAHubViewModel,FragmentAddAHubBinding>() {
+
+    private  val addAHubViewModel: AddAHubViewModel by inject()
+    override fun getViewModel(): AddAHubViewModel {
+            return addAHubViewModel
+    }
+
+    override fun getLayoutResourceId(): Int {
+        return R.layout.fragment_add_a_hub
+    }
+
+    override fun initView() {
+        super.initView()
+    }
+
+
+
+}
