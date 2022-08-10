587ff6a732167b7d387ecc90bfb466f8835ae45c
bharat kumar <bharat.kumar@risertechub.in>
2022-08-10 20:23:30 +0530

    Fragment Edit and Add Sensors

--- app/src/main/java/com/hbeonlabs/smartguard/ui/fragments/editHub/FragmentPostAddHub.kt
+++ app/src/main/java/com/hbeonlabs/smartguard/ui/fragments/editHub/FragmentPostAddHub.kt
@@ -1,31 +1,30 @@
 package com.hbeonlabs.smartguard.ui.fragments.editHub
 
+import android.view.View
 import com.hbeonlabs.smartguard.R
 import com.hbeonlabs.smartguard.base.BaseFragment
-import com.hbeonlabs.smartguard.databinding.FragmentEditHubAddSensorsBinding
+import com.hbeonlabs.smartguard.databinding.FragmentAddHubPostVerificationBinding
+import com.hbeonlabs.smartguard.ui.activities.MainActivity
 
 import org.koin.android.ext.android.inject
 
 
-class FragmentEditHub:BaseFragment<EditHubViewModel,FragmentEditHubAddSensorsBinding>() {
+class FragmentPostAddHub:BaseFragment<PostAddHubViewModel,FragmentAddHubPostVerificationBinding>() {
 
-    private  val editHubViewModel: EditHubViewModel by inject()
-    override fun getViewModel(): EditHubViewModel {
-            return editHubViewModel
+    private  val postAddHubViewModel: PostAddHubViewModel by inject()
+    override fun getViewModel(): PostAddHubViewModel {
+            return postAddHubViewModel
     }
 
     override fun getLayoutResourceId(): Int {
-        return R.layout.fragment_edit_hub_add_sensors
+        return R.layout.fragment_add_hub_post_verification
     }
 
     override fun initView() {
         super.initView()
 
-/*
         (requireActivity() as MainActivity).binding.toolbarIconEnd.setImageResource(R.drawable.ic_baseline_help)
         (requireActivity() as MainActivity).binding.toolbarIconStart.visibility = View.GONE
-*/
-
 
     }
 
--- app/src/main/java/com/hbeonlabs/smartguard/ui/fragments/editHub/PostAddHubViewModel.kt
+++ app/src/main/java/com/hbeonlabs/smartguard/ui/fragments/editHub/PostAddHubViewModel.kt
@@ -2,7 +2,7 @@ package com.hbeonlabs.smartguard.ui.fragments.editHub
 
 import com.hbeonlabs.smartguard.base.BaseViewModel
 
-class EditHubViewModel:BaseViewModel()
+class PostAddHubViewModel:BaseViewModel()
 {
 
 }
